5a448d2be2eff98746ce213e85e373c6e3306de5
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2024-07-03 19:42:21 +0000

    introduced stack navigation

--- assignment-6/App.js
+++ assignment-6/App.js
@@ -1,14 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
 
 import Shop from './components/Shop'
+import CartBar from './components/CartBar'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Shop/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Shop'>
+        <Stack.Screen name="Shop" component={Shop} />
+        <Stack.Screen name="CartBar" component={CartBar} />
+      </Stack.Navigator>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
